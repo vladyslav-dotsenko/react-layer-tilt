ffbe46c4459545dd3c53a5126506ec1c895ececf
Vlad Dotsenko <karponter.tarino@gmail.com>
2021-11-04 23:49:39 +0200

    fix. build gh pages script concurency fix

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,9 +1,10 @@
-git checkout master;
-git branch -D gh-pages;
-git branch gh-pages;
-git checkout gh-pages;
-yarn build;
-rm -rf LICENSE node_modules package.json public README.md src yarn.lock build-gh-pages.sh;
-mv build/* ./;
-rm -rf build;
-git push --force -u origin gh-pages;
+git checkout master &&
+git branch -D gh-pages &&
+git branch gh-pages &&
+git checkout gh-pages &&
+yarn install &&
+yarn build &&
+rm -rf LICENSE node_modules package.json public README.md src yarn.lock build-gh-pages.sh &&
+mv build/* ./ &&
+rm -rf build &&
+git push --force -u origin gh-pages &&
